568d7a8938895388ab7e2868e2dfa3c178f23dc6
Joas Assunção <joassalvior@gmail.com>
2020-11-19 12:13:07 -0300

    Curso JavaScript

---
+++ aula12/ex012.js
@@ -0,0 +1,16 @@
+var agora = new Date() 
+var hora = agora.getHours()
+var min = agora.getMinutes()
+var seg = agora.getUTCSeconds()
+console.log (`Agora são exatamente ${hora}:${min}:${seg}.`)
+if (hora < 6 ) {
+    console.log('Boa Madrugada!')
+} else if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora < 18) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
+
+//condição aninhada
